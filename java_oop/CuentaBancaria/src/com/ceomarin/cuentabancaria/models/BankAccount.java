8f6e08e955b10b93d75b41b4b8847dd0f12362dc
ceomarin <cmarin.ing@gmail.com>
2023-05-07 23:03:01 -0400

    format code project CuentaBancaria

--- java_oop/CuentaBancaria/src/com/ceomarin/cuentabancaria/models/BankAccount.java
+++ java_oop/CuentaBancaria/src/com/ceomarin/cuentabancaria/models/BankAccount.java
@@ -1,13 +1,12 @@
 package com.ceomarin.cuentabancaria.models;
 
-
 public class BankAccount {
 	private String numeroCuenta;
 	private double saldoCuentaCorriente;
 	private double saldoCuentaAhorro;
 	private static int contadorCuentas;
 	private static double dineroAlmacenado;
-	
+
 	public BankAccount() {
 		this.numeroCuenta = generarNumeroCuenta();
 		this.saldoCuentaCorriente = 0;
@@ -15,79 +14,82 @@ public class BankAccount {
 		BankAccount.contadorCuentas++;
 		BankAccount.dineroAlmacenado = 0;
 	}
-	
+
 	private String generarNumeroCuenta() {
-		String result ="";
+		String result = "";
 		try {
-			for	(int i = 0; i < 10; i++) {
-				int numInt = (int)(Math.random()*10);
+			for (int i = 0; i < 10; i++) {
+				int numInt = (int) (Math.random() * 10);
 				result += Integer.toString(numInt);
-			}	
-		}catch (Exception e) {
-			System.out.println("Excepcion: "+e);
+			}
+		} catch (Exception e) {
+			System.out.println("Excepcion: " + e);
 		}
 
 		return result;
 	}
-	
+
 	public String getNumeroCuenta() {
 		return this.numeroCuenta;
 	}
-	
+
 	public void setNumeroCuenta(String numeroCuenta) {
 		this.numeroCuenta = numeroCuenta;
 	}
-	
+
 	public double getSaldoCuentaCorriente() {
 		return this.saldoCuentaCorriente;
 	}
+
 	public void setSaldoCuentaCorriente(double saldoCuentaCorriente) {
 		this.saldoCuentaCorriente = saldoCuentaCorriente;
 	}
-	
+
 	public double getSaldoCuentaAhorro() {
 		return this.saldoCuentaAhorro;
-	}	
+	}
+
 	public void setSaldoCuentaAhorro(double saldoCuentaAhorro) {
 		this.saldoCuentaAhorro = saldoCuentaAhorro;
 	}
-	
+
 	public int getContadorCuentas() {
 		return BankAccount.contadorCuentas;
 	}
-	
+
 	public double getDineroAlmacenado() {
 		return BankAccount.dineroAlmacenado;
 	}
-	
+
 	public void depositarDinero(String tipoCuenta, double deposito) {
 		double total = 0;
-		if(tipoCuenta.equalsIgnoreCase("corriente") && deposito > 0) { 
+		if (tipoCuenta.equalsIgnoreCase("corriente") && deposito > 0) {
 			total = getSaldoCuentaCorriente() + deposito;
 			setSaldoCuentaCorriente(total);
 		}
-		if(tipoCuenta.equalsIgnoreCase("ahorro") && deposito > 0) {
+		if (tipoCuenta.equalsIgnoreCase("ahorro") && deposito > 0) {
 			total = getSaldoCuentaAhorro() + deposito;
 			setSaldoCuentaAhorro(total);
 		}
 		BankAccount.dineroAlmacenado = getSaldoCuentaAhorro() + getSaldoCuentaCorriente();
 	}
+
 	public void retirarDinero(String tipoCuenta, double retiro) {
 		double total = 0;
-		if(tipoCuenta.equalsIgnoreCase("corriente") && retiro > 0) {
-			if(retiro < getSaldoCuentaCorriente()) {
+		if (tipoCuenta.equalsIgnoreCase("corriente") && retiro > 0) {
+			if (retiro < getSaldoCuentaCorriente()) {
 				total = getSaldoCuentaCorriente() - retiro;
-				setSaldoCuentaCorriente(total);	
-			}else {
+				setSaldoCuentaCorriente(total);
+			} else {
 				System.out.println("Saldo insuficiente");
 			}
-			
+
 		}
-		if(tipoCuenta.equalsIgnoreCase("ahorro") && retiro > 0) {
-			if(retiro < getSaldoCuentaAhorro()) {
+		if (tipoCuenta.equalsIgnoreCase("ahorro") && retiro > 0) {
+			if (retiro < getSaldoCuentaAhorro()) {
 				total = getSaldoCuentaAhorro() - retiro;
-				setSaldoCuentaAhorro(total);	
-			}else {
+				setSaldoCuentaAhorro(total);
+			} else {
 				System.out.println("Saldo insuficiente");
 			}
 		}
