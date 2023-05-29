cadcb4baca716ee87757e2b42b5ac88947f23cde
ceomarin <cmarin.ing@gmail.com>
2023-05-29 00:38:05 -0400

    add bookapi

---
+++ spring-data/bookapi/src/main/java/com/ceomarin/bookapi/services/BookService.java
@@ -0,0 +1,60 @@
+package com.ceomarin.bookapi.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.ceomarin.bookapi.models.Book;
+import com.ceomarin.bookapi.repositories.BookRepository;
+
+@Service
+public class BookService {
+	// Agregando el book al repositorio como una dependencia
+	private final BookRepository bookRepository;
+
+	public BookService(BookRepository bookRepository) {
+		this.bookRepository = bookRepository;
+	}
+
+	// Devolviendo todos los libros.
+	public List<Book> allBooks() {
+		return bookRepository.findAll();
+	}
+
+	// Creando un libro.
+	public Book createBook(Book b) {
+		return bookRepository.save(b);
+	}
+
+	// Obteniendo la información de un libro
+	public Book findBook(Long id) {
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if (optionalBook.isPresent()) {
+			return optionalBook.get();
+		} else {
+			return null;
+		}
+	}
+	// actualizar la información de un libro
+	public Book updateBook(Long id, String title, String desc, String lang, Integer numOfPages) {
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if (optionalBook.isPresent()) {
+			Book bookToUpdate = optionalBook.get();
+			bookToUpdate.setTitle(title);
+			bookToUpdate.setDescription(desc);
+			bookToUpdate.setLanguage(lang);
+			bookToUpdate.setNumberOfPages(numOfPages);
+
+			bookRepository.save(bookToUpdate);
+			return bookToUpdate;
+		} else {
+			return null;
+		}
+	}
+
+	// Eliminar un libro
+	public void deleteBook(Long id) {
+		bookRepository.deleteById(id);
+	}
+}
